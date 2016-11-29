65051e854c38c1be1972944d02c0ed7206a17e41
MervinPraison <praison.ms@gmail.com>
2016-11-29 13:45:00 +0000

    Working up to single page update

--- ajax-form.php
+++ ajax-form.php
@@ -37,6 +37,8 @@ function ajax_form_list_display() {
 	    'hide_empty' => true,
 	) );
 
+	$category_text ='';
+
 	$category_text .= '<ul>';
 	foreach ($terms as $term) {
 		$term_ids[] = $term->term_id;
@@ -50,9 +52,17 @@ function ajax_form_list_display() {
 
 
 
+/*if ( ! function_exists( 'ajax_form_category_single' ) ) {  // Not if function exists Start
+// Ajax form start
+function ajax_form_category_single($args){
+
 	// Start of Category Form for the current post
 
+	if(isset($args['id'])) {
+		$cat_id = $args['id'];
+	}
 	
+	$value = '';	
 
 		$prev_cat_id = get_post_meta( get_the_ID(), 'form_cat', true );
 		$term = get_term($prev_cat_id);
@@ -81,16 +91,13 @@ function ajax_form_list_display() {
 			'value_field'	     => 'term_id',
 		);
 
-		$value ='
-		<form class="form" method="post" id="ajax-form-list" action="/aah/wp-admin/admin-ajax.php">
+		$value =get_the_title().' 
+		:<form class="form" method="post" id="ajax-form-list" action="/aah/wp-admin/admin-ajax.php">
 		<input type="hidden" name="action" value="form_list">
 		<input type="hidden" name="post_id" id="post_id" value="'.get_the_ID().'">';
 		$value .= wp_dropdown_categories($args);
 		//$value .= wp_nonce_field( 'contact_form_nonce_2', 'name_of_nonce_field' );
 		$value .= '<button type="submit" class="btn">Submit</button></form>';
-
-
-
 
 		$value .= '<br /><span id="cat-form-output">'.$term->name.'</span>';
 
@@ -98,9 +105,14 @@ function ajax_form_list_display() {
 	// End of Category Form for the current post
 
 
+} // End of ajax_form_category_single function exists check
+
+} // End of ajax_form_category_single*/
 
 
-	} // End of Is single
+
+
+} // End of Is single
 
 
 		
@@ -125,32 +137,107 @@ function form_list()
         exit('The form is not valid'); 
     }
  
+
+if ( ! function_exists( 'ajax_form_category_single' ) ) {  // Not if function exists Start
+// Ajax form start
+function ajax_form_category_single($args){
+
+	// Start of Category Form for the current post
+
+	if(isset($args['catid'])) {
+		$cat_id = $args['catid'];
+	}
+	if (isset($args['postid'])) {
+		$post_id = $args['postid'];
+	}
+
+	$value = '';	
+
+		//$prev_cat_id = get_post_meta( get_the_ID(), 'form_cat', true );
+		$term = get_term($cat_id);
+
+		$args = array(
+			'show_option_all'    => '',
+			'show_option_none'   => '',
+			'option_none_value'  => '-1',
+			'orderby'            => 'ID',
+			'order'              => 'ASC',
+			'show_count'         => 0,
+			'hide_empty'         => 0,
+			'child_of'           => 0,
+			'exclude'            => '',
+			'include'            => '',
+			'echo'               => 0,
+			'selected'           => $cat_id,
+			'hierarchical'       => 0,
+			'name'               => 'cat_name',
+			'id'                 => 'cat_name',
+			'class'              => 'cat_name',
+			'depth'              => 0,
+			'tab_index'          => 0,
+			'taxonomy'           => 'category',
+			'hide_if_empty'      => false,
+			'value_field'	     => 'term_id',
+		);
+		
+		// /aah/wp-admin/admin-ajax.php 
+		//$title = get_the_title();
+		$value ='<td><form class="form" method="post" id="ajax-form-list" action="/aah/wp-admin/admin-ajax.php ">
+		<input type="hidden" name="action" value="form_list">
+		<input type="hidden" name="post_id" id="post_id" value="'.$post_id.'">';
+		$value .= wp_dropdown_categories($args);
+		//$value .= wp_nonce_field( 'contact_form_nonce_2', 'name_of_nonce_field' );
+		$value .= '<button type="submit" class="btn">Submit</button></form></td><td>';
+
+		$value .= '<span id="cat-form-output">'.$term->name.'</span></td>';
+		return $value;
+
+
+	// End of Category Form for the current post
+
+
+} // End of ajax_form_category_single function exists check
+
+} // End of ajax_form_category_single
+
+
+
     // ... Processing further
 
-	$cat_name = $_REQUEST['cat_name']; 
+	$cat_name = $_REQUEST['cat_name']; // Updating Individual post category (Ajax JS 1 )
+	$category = $_REQUEST['category_name']; // Listing / retrieving post's under a category (Ajax JS 2)
 	$post_id = $_REQUEST['post_id'];
 
 	if($cat_name&&$post_id) {
 		wp_set_post_categories( $post_id, $cat_name, false );
 	}
 
-	$term = get_term($cat_name);
+	if($cat_name){
+		$term = get_term($cat_name);
+	}
 
-	$category = $_REQUEST['cat_name'];
-	$args = array( 'posts_per_page' => 1000, 'order'=> 'ASC', 'orderby' => 'title', 'category' => $category );
-	$postslist = get_posts( $args );
+	if($category){
+		
+		$args = array( 'posts_per_page' => 100, 'order'=> 'ASC', 'orderby' => 'title', 'category' => $category );
+		$postslist = get_posts( $args );
 
-	$response .= '<h2>'.get_the_category_by_ID($category[0]).'</h2><ul>';
-	foreach ( $postslist as $post ) :
-  	setup_postdata( $post ); 
-		$response .= '<li>'.$post->post_title.'</li>';
-	endforeach; 
-	$response .= '</ul>';
-	wp_reset_postdata();
+
+		$response .= '<h2>'.get_the_category_by_ID($category[0]).'</h2><ul>';
+		//print_r($postslist);
+		foreach ( $postslist as $post ) :
+	  	setup_postdata( $post ); 
+	  		$cat_form_args = array('catid'=> $category, 'postid' => $post->ID );
+			$response .= '<table><tr><td>'.$post->post_title.'</td>'.ajax_form_category_single($cat_form_args).'</tr></table>';
+		endforeach; 	
+		$response .= '</ul>';
+
+		wp_reset_postdata();
+	}
 
 
 	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) { 
-		echo $term->name. $response .' <span style="color:LightGreen;" >Updated</span>';
+		if(!$category)echo $term->name. $response .' <span style="color:LightGreen;" >Updated</span>';
+		else echo $response;
 		die();
 	}
 	else {
--- form.js
+++ form.js
@@ -1,30 +1,8 @@
 jQuery(document).ready(function($) {
 
-jQuery('#ajax-form-list').submit(function(e){
-    var cat_name = jQuery("select#cat_name option:selected").val();
-    var post_id = jQuery("#post_id").val();
-    jQuery.ajax({ 
-         url: catformlist.ajax_url,         
-		 type: 'post',
-         data: {
-         		action: 'form_list', 
-         		cat_name:cat_name,
-         		post_id:post_id,
-         		security: catformlist.ajax_nonce
-         },
-         success: function(response) {
-              jQuery('#cat-form-output').html( response ); //should print out the name since you sent it along
-
-        }
-    });
-
-    return false;
-
-});
-
 
 jQuery( document ).on( 'click', '#cat-list-button-id', function() {
-    var post_id = jQuery(this).data('id-list'); 
+    var post_id = jQuery(this).data('id-list');
     var category_name = jQuery(this).data('category-list');
     jQuery.ajax({
         url : catformlist.ajax_url,
@@ -37,10 +15,36 @@ jQuery( document ).on( 'click', '#cat-list-button-id', function() {
         },
         success : function( response ) {
             jQuery('#cat-display').html( response );
+
+                //jQuery(document).ready(function($) {
+                    jQuery('#ajax-form-list').submit(function(e){
+                    var cat_name = jQuery("select#cat_name option:selected").val();
+                    var post_id = jQuery("#post_id").val();
+                    jQuery.ajax({
+                         url: catformlist.ajax_url,
+                         type: 'post',
+                         data: {
+                                action: 'form_list',
+                                cat_name:cat_name,
+                                post_id:post_id,
+                                security: catformlist.ajax_nonce
+                         },
+                         success: function(response) {
+                              jQuery('#cat-form-output').html( response ); //should print out the name since you sent it along
+
+                        }
+                    });
+
+                    return false;
+
+                    });
+
+                 //});
         }
     });
 
     return false;
 });
+
 
 });
